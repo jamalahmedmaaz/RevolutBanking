01342812df00e624384ea8be9cb20ba02aa3b7c9
Jamal <jamalahmedmaaz@gmail.com>
2019-07-13 17:33:04 +0530

    Some more documentation

--- src/main/java/com/revolut/model/AccountType.java
+++ src/main/java/com/revolut/model/AccountType.java
@@ -1,6 +1,22 @@
 package com.revolut.model;
 
 /**
+ * We can lot of account types.
+ * <p>
+ * Ex: Crypto etc.
+ * <p>
+ * For that we need to add validations
+ * <p>
+ * Ex: Transferring between Personal to Personal
+ * or Business to Person and vices-versa are
+ * possible and crypto to crypto is possible
+ * but not between cryto and others and vices versa
+ * <p>
+ * Keeping it simple, but you can scale the Product
+ * with adding these new types and adding validations.
+ */
+
+/**
  * The enum Account type.
  */
 public enum AccountType {
